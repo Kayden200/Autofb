1e967f875d80d482bac8f0d41cd157ae0cb4a784
Kayden200 <159207295+Kayden200@users.noreply.github.com>
2025-04-01 12:06:50 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 gunicorn -b 0.0.0.0:10000 fb_auto_create_backend:app
+
+#!/bin/bash
+Xvfb :99 -screen 0 1920x1080x24 &
+export DISPLAY=:99
+gunicorn -b 0.0.0.0:10000 fb_auto_create_backend:app
